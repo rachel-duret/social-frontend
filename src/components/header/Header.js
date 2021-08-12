c868a2bdbf4a95ce82a8f1a0c77199b3f204e463
rachelduret <chuncheung.ku@gmail.com>
2021-08-12 17:02:20 +0200

    add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './header.scss'
+import {Search, Person, Chat, NotificationsActive } from '@material-ui/icons';
+/* import {BrowerRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom' */
+
+function Header() {
+    return (
+        <div className="headerContainer">
+            <div className="headerLeft">
+                <span className="logo">On patage</span>
+                
+            </div>
+
+            <div className="headerCenter">
+                <div className="searchBar">
+                    <Search classname="searchIcon" />
+                    <input type="text" className="searchInput" placeholder="Search for friend or post ..." />
+                </div>
+            </div>
+
+            <div className="headerRight">
+                <div className="headerLinks">
+                    <span className="headerLink">Home Page</span>
+                    <span className="headerLink">Timeline</span>     
+                </div>
+                <div className="headerIcons">
+                    <div className="headerIconItem">
+                        <Person />
+                        <span className="headerIconBadge">1</span>
+                    </div>
+                    <div className="headerIconItem">
+                        <Chat />
+                        <span className="headerIconBadge">1</span>
+                    </div>
+                    <div className="headerIconItem">
+                        <NotificationsActive />
+                        <span className="headerIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="assets/photo1.jpg" alt="" className="headerImg" />
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
